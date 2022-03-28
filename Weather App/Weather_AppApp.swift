61584036040c6f17af6acd4a641fa6bef78c0f5c
Baso <baso.ahmad@ruangguru.com>
2022-03-28 18:35:16 +0700

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Ruangguru on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
